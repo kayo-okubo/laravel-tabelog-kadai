fc1d9d3cad7916a118eaa0b6013ba3777baae3af
yasu6kayo <3255kynyan@gmail.com>
2026-08-27 15:03:26 +0900

    管理者認証機能を追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,52 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>NAGOYAMESHI</title>
 
-        <!-- Font Awesome -->
-         <script src="https://kit.fontawesome.com/0f0d73bdc9.js" crossorigin="anonymous"></script>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
 
-         <link href="{{ asset('css/tabelog.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <div id="app">
+    @stack('fonts')
 
-            @component('components.header')
-            @endcomponent
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
-            <main class="py-4 mb-5">
+    <link href="{{ asset('/css/nagoyameshi.css') }}" rel="stylesheet">
+
+    @stack('styles')
+</head>
+<body>
+    <div id="app" class="nagoyameshi-wrapper">
+        @include('layouts.header')
+
+        <main>
+            @if (Auth::guard('admin')->check())
+                <div class="container py-4 nagoyameshi-container">
+                    <div class="row justify-content-center">
+                        @include('layouts.sidebar')
+                        @yield('content')
+                    </div>
+                </div>
+            @else
                 @yield('content')
-            </main>
+            @endif
+        </main>
 
-            @component('components.footer')
-            @endcomponent
-        </div>
+        @include('components.footer')
+    </div>
 
-        <!-- Scripts -->
-         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    </body>
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    @stack('scripts')
+</body>
 </html>
