f026e3e9251fe4e8bbfce874a2f896818205327e
yasu6kayo <3255kynyan@gmail.com>
2026-08-29 16:37:12 +0900

    Herokuのプロキシ設定を追加

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->trustProxies(
+        at: '*',
+        headers: Request::HEADER_X_FORWARDED_FOR |
+                    Request::HEADER_X_FORWARDED_HOST |
+                    Request::HEADER_X_FORWARDED_PORT |
+                    Request::HEADER_X_FORWARDED_PROTO |
+                    Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         $middleware->redirectUsersTo(fn () => Auth::guard('admin')->check() ? '/admin/home' : '/');
 
         $middleware->redirectGuestsTo(fn ($request) => $request->is('admin/*') ? route('admin.login') : route('login'));
