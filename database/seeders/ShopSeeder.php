6a88552cd8e7e8489c434e7dd18d4b13e3142189
yasu6kayo <3255kynyan@gmail.com>
2026-08-29 15:20:35 +0900

    本番環境用の店舗Seederを修正

--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -13,6 +13,16 @@ class ShopSeeder extends Seeder
      */
     public function run(): void
     {
-        Shop::factory()->count(20)->create();
+        for ($i = 1; $i <= 20; $i++) {
+            Shop::create([
+                'name' => '店舗' . $i,
+                'description' => '店舗の説明です。',
+                'address' => '佐賀県佐賀市',
+                'phone_number' => '0952-00-0000',
+                'regular_holiday' => '不定休',
+                'business_hours' => '11:00〜20:00',
+                'price_range' => '3,000円〜5,000円',
+                'category_id' => (($i - 1) % 10) + 1,
+            ]);
     }
 }
