9bff2a319f2a107d82a15b87d2377af90a1f4461
yasu6kayo <3255kynyan@gmail.com>
2026-08-30 16:45:13 +0900

    有料会員画面の表示を調整

--- resources/views/subscription/cancel.blade.php
+++ resources/views/subscription/cancel.blade.php
@@ -13,18 +13,17 @@
 
                 <h1 class="mb-3 text-center">有料プラン解約</h1>
 
-                <p>有料プランを解約すると以下の特典を受けられなくなります。本当に解約してもよろしいですか？</p>
+                <p>有料プランを解約すると以下の特典を受けられなくなります。<br>本当に解約してもよろしいですか？</p>
 
                 <div class="card mb-4">
                     <div class="card-header text-center">
                         有料プランの内容
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">・当日の2時間前までならいつでも予約可能</li>
-                        <li class="list-group-item">・店舗をお好きなだけお気に入りに追加可能</li>
-                        <li class="list-group-item">・レビューを全件閲覧可能</li>
-                        <li class="list-group-item">・レビューを投稿可能</li>
-                        <li class="list-group-item">・月額たったの300円</li>
+                        <li class="list-group-item">・店舗を予約できます</li>
+                        <li class="list-group-item">・店舗をお気に入りに追加できます</li>
+                        <li class="list-group-item">・レビューを投稿・編集・削除できます</li>
+                        <li class="list-group-item">・月額300円</li>
                     </ul>
                 </div>
 
@@ -32,7 +31,7 @@
                     @csrf
                     @method('delete')
                     <div class="form-group d-flex justify-content-center">
-                        <button class="btn text-white shadow-sm w-50 nagoyameshi-btn-danger">解約</button>
+                        <button class="btn btn-danger shadow-sm w-50">解約</button>
                     </div>
                 </form>
             </div>
--- resources/views/subscription/create.blade.php
+++ resources/views/subscription/create.blade.php
@@ -9,7 +9,7 @@
 @endpush
 
 @section('content')
-    <div class="container nagoyameshi-container pb-5">
+    <div class="container pb-5">
         <div class="row justify-content-center">
             <div class="col-xl-5 col-lg-6 col-md-8">
                 <nav class="my-3" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
@@ -32,28 +32,35 @@
                         有料プランの内容
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">・当日の2時間前までならいつでも予約可能</li>
-                        <li class="list-group-item">・店舗をお好きなだけお気に入りに追加可能</li>
-                        <li class="list-group-item">・レビューを全件閲覧可能</li>
-                        <li class="list-group-item">・レビューを投稿可能</li>
-                        <li class="list-group-item">・月額たったの300円</li>
+                        <li class="list-group-item">・店舗を予約できます</li>
+                        <li class="list-group-item">・店舗をお気に入りに追加できます</li>
+                        <li class="list-group-item">・レビューを投稿・編集・削除できます</li>
+                        <li class="list-group-item">・月額300円</li>
                     </ul>
                 </div>
 
                 <hr class="mb-4">
 
-                <div class="alert alert-danger nagoyameshi-card-error" id="card-error" role="alert">
+                <div class="alert alert-danger d-none" id="card-error" role="alert">
                     <ul class="mb-0" id="error-list"></ul>
                 </div>
 
                 <form id="card-form" action="{{ route('subscription.store') }}" method="post">
                     @csrf
-                    <input class="nagoyameshi-card-holder-name mb-3" id="card-holder-name" type="text" placeholder="カード名義人" required>
-                    <div class="nagoyameshi-card-element mb-4" id="card-element"></div>
+                    <input class="form-control mb-3"
+                        id="card-holder-name"
+                        type="text"
+                        placeholder="カード名義人"
+                        required>
+                    <div class="form-control mb-4 py-3" id="card-element"></div>
 
                 </form>
                 <div class="d-flex justify-content-center">
-                    <button class="btn text-white shadow-sm w-50 nagoyameshi-btn" id="card-button" data-secret="{{ $intent->client_secret }}">登録</button>
+                    <button class="btn btn-dark shadow-sm w-50"
+                        id="card-button" 
+                        data-secret="{{ $intent->client_secret }}">
+                        登録
+                    </button>
                 </div>
             </div>
         </div>
